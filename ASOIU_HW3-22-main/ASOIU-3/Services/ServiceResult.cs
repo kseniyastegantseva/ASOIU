86d777ca5d9ebbc5c822e3b15d3b88b40d9ee0ce
Kseniya Stegantseva <120799112+kseniyastegantseva@users.noreply.github.com>
2026-06-07 20:52:02 +0300

    Delete ASOIU_HW3-22-main directory

--- ASOIU_HW3-22-main/ASOIU-3/Services/ServiceResult.cs
+++
@@ -1,8 +0,0 @@
-namespace ASOIU_3.Services;
-
-internal readonly record struct ServiceResult(bool IsSuccess, string Message)
-{
-    public static ServiceResult Ok(string message) => new(true, message);
-
-    public static ServiceResult Fail(string message) => new(false, message);
-}
